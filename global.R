0347b385f161ae67e820672855de4a91965104cc
Ricardo Ochoa Sosa <ochoasosaricardo@gmail.com>
2018-11-08 17:30:46 -0600

    Added FTP connection functions

--- global.R
+++ global.R
@@ -1,6 +1,12 @@
 # Libraries
 # global.R
 
+# FTP
+ftp <- "your ftp address" # Please update with your own info
+usr <- "your username"    # Please update with your own info
+pas <- "your password"    # Please update with your own info
+data_folder <- paste0("ftp://", usr, ":", pas, "@", ftp)
+
 # Data
 ## shiny
 library(shiny)
@@ -20,6 +26,9 @@ library(waffle)
 library(readr)
 library(markdown)
 library(reshape2)
+
+# connections
+library(RCurl)
 
 # Functions
 source("functions.R")
